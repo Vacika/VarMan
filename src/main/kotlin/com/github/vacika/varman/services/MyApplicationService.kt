25a1e5c3569bc3d79981e85cd4a2fdd106eda566
GitHub Action <action@github.com>
2022-09-09 08:14:51 +0000

    Template cleanup

--- src/main/kotlin/com/github/vacika/varman/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/vacika/varman/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.vacika.varman.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.vacika.varman.MyBundle
 
 class MyApplicationService {
 
--- src/test/kotlin/com/github/vacika/varman/MyPluginTest.kt
+++ src/test/kotlin/com/github/vacika/varman/MyPluginTest.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.vacika.varman
 
 import com.intellij.ide.highlighter.XmlFileType
 import com.intellij.psi.xml.XmlFile
